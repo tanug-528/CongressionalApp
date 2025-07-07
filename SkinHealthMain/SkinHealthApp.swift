39cec88af9cf37556cbda529084dbd7bd43e985b
Tanu <gtanu568@gmail.com>
2025-07-07 12:53:27 -0700

    add new folder

---
+++ SkinHealthMain/SkinHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinHealthApp.swift
+//  SkinHealth
+//
+//  Created by Tanu Goyal on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SkinHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomePage()
+        }
+    }
+}

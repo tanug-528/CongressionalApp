89fbe131f9e643829bcfeb3a500789bef88e61b4
Tanu <gtanu568@gmail.com>
2025-07-07 12:42:48 -0700

    Initial commit

---
+++ SkinHealthTests/SkinHealthTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkinHealthTests.swift
+//  SkinHealthTests
+//
+//  Created by Tanu Goyal on 7/4/25.
+//
+
+import Testing
+@testable import SkinHealth
+
+struct SkinHealthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
